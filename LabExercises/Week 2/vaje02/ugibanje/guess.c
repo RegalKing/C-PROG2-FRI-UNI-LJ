2b429662cccea31cebce1206f134efd4aa0c69de
RegalKing <alexperkoplavec@gmail.com>
2023-04-25 02:47:52 +0200

    Week 2 done

--- LabExercises/Week 2/vaje02/ugibanje/guess.c
+++ LabExercises/Week 2/vaje02/ugibanje/guess.c
@@ -1,11 +1,39 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <stdbool.h>
-#include <ctype.h>
 #include <limits.h>
 
-int main (){ 
+int main()
+{
+    int floor;
+    int ceil;
+    scanf("%d%d", &floor, &ceil);
 
-    
+    int odgovor=INT_MIN;
+    int guess=INT_MIN;
+    int jeProtislovje=0;
 
+    while (odgovor!=0){
+        guess=(floor+ceil)/2;
+        scanf("%d",&odgovor);
+
+        if (odgovor == 1){
+            floor=guess+1;
+        }
+        else if (odgovor == -1){
+            ceil=guess-1;
+        }
+        if (floor>ceil || ceil<floor){
+            printf("PROTISLOVJE");
+            break;
+        }
+        if (odgovor==0){
+            if (floor==ceil){
+                printf("%d",ceil);
+            }
+            else{
+                printf("%d %d",floor,ceil);
+            }
+            break;
+        }
+    }
 }
