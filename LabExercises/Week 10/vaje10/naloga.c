7568b6b33c13a648636c6a94868c827b585f1daa
RegalKing <alexperkoplavec@gmail.com>
2023-05-12 06:56:52 +0200

    finalized lab exercises week 10 with some comments in 3rd task

--- LabExercises/Week 10/vaje10/naloga.c
+++ LabExercises/Week 10/vaje10/naloga.c
@@ -27,13 +27,47 @@ int poisciPO(Student* student, char* predmet) {
 }
 
 
-        
+// za funkcijo "dodaj" razlaga:
+// 1. moznost) ce student z vpisno se ne obstaja -> dodamo novega studenta na mesto v "studentje" ki je NULL (potem moramo po njem dodati tudi nov NULL), zanj dodamo da je ta predmet naredil z to oceno
+
+// 2. moznost) student ze obstaja, vendar predmeta se ni delal -> dodamo na konec PO (v prvo prazno celico) njegov predmet in oceno
+
+// 3. moznost) student obstaja in ta student je ze delal predmet -> novo oceno vpisi v del ocena od predmeta
+
 
 int dodaj(Student** studentje, int stStudentov, int vpisna, char* predmet, int ocena) {
     
-    for (int i=0; i<stStudentov; i++){
-        
+    int indexStudenta = poisciStudenta(studentje, stStudentov, vpisna);
 
+    if (indexStudenta == -1){ // student v tabeli "studentje" se ne obstaja, zato ustvarimo novo strukturo
+                              // in kazalec nanjo vpisemo v prvo prosto mesto v tabeli
+        Student* novStudent = malloc(sizeof(Student));
+
+        novStudent->vpisna=vpisna;
+        novStudent->stPO=1;
+        novStudent->po=malloc(10 * sizeof(PO));
+
+        strcpy(novStudent->po[0].predmet , predmet);
+        novStudent->po[0].ocena=ocena;
+
+        studentje[stStudentov]=novStudent;
+        stStudentov++;
+    }
+    else{ // student obstaja v tabeli "studentje"
+        Student* stud=studentje[indexStudenta]; // pointer na studenta, da bo manj za pisat
+
+        int indexPredmeta = poisciPO(stud , predmet);
+
+        if (indexPredmeta == -1){ // predmeta se ni pri studentu 
+            strcpy(stud->po[stud->stPO].predmet , predmet);
+            stud->po[stud->stPO].ocena=ocena;
+            stud->stPO++;
+        }
+        else{ // student je predmet ze opravljal
+         stud->po[indexPredmeta].ocena=ocena;
+        }
+    }
+    return stStudentov;
 }
 
 #ifndef test
