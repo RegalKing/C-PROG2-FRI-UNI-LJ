2a89ff84fc4a442aba0d59eb1fe35ffa5d450f66
RegalKing <alexperkoplavec@gmail.com>
2023-05-24 22:37:50 +0200

    solved structures, linked lists tasks for 2018 midterms

---
+++ IzpKol/IzpKol2018/2.KolokvijA/kolokvij2_javniTestiA/naloga1/naloga1.c
@@ -0,0 +1,36 @@
+
+/*
+ * Va"sa re"sitev prve naloge --- datoteko dopolnite in jo oddajte na spletno u"cilnico!
+ *
+ * Your solution to task 1 --- complete the file and submit it to U"cilnica!
+ *
+ * V naslednjo vrstico vpi"site va"so vpisno "stevilko / Enter your student ID number in the next line:
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+#include "naloga1.h"
+
+int steviloElementov(Vozlisce* p) {
+    
+    Vozlisce* temp = p;
+    int sum=0;
+    do {
+        temp=temp->naslednje;
+        sum++;
+    } while (p!=temp);
+
+    return sum;
+
+
+}
+
+int main() {
+    // po "zelji dodajte kodo za ro"cno testiranje ...
+    // add manual testing code if desired ...
+    return 0;
+}
